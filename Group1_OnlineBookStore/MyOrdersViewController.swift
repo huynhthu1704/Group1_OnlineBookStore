4135c81266438ab9eed27b1ebce47d84953c7303
Pham Van Thanh <robertthanh1107@gmail.com>
2022-05-15 15:34:24 +0700

    create my orders screen by Thanh

--- Group1_OnlineBookStore/MyOrdersViewController.swift
+++ Group1_OnlineBookStore/MyOrdersViewController.swift
@@ -1,26 +1,21 @@
 //
-//  HomeScreenViewController.swift
+//  MyOrdersViewController.swift
 //  Group1_OnlineBookStore
 //
-//  Created by Huynh Thu on 2022-05-14.
+//  Created by Pham Van Thanh on 2022-05-15.
 //  Copyright © 2022 Huynh Thu. All rights reserved.
 //
 
 import UIKit
 
-class HomeScreenViewController: UIViewController {
+class MyOrdersViewController: UIViewController {
 
-    @IBAction func button(_ sender: UIButton) {
-
-        let myViewController = BookDetailViewController(nibName: "BookDetailViewController", bundle: nil)
-        self.present(myViewController, animated: true, completion: nil)
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+
 
     /*
     // MARK: - Navigation
--- Group1_OnlineBookStore/ProfileScreenViewController.swift
+++ Group1_OnlineBookStore/ProfileScreenViewController.swift
@@ -1,27 +1,27 @@
 //
-//  HomeScreenViewController.swift
+//  ProfileScreenViewController.swift
 //  Group1_OnlineBookStore
 //
-//  Created by Huynh Thu on 2022-05-14.
+//  Created by Pham Van Thanh on 2022-05-15.
 //  Copyright © 2022 Huynh Thu. All rights reserved.
 //
 
 import UIKit
 
-class HomeScreenViewController: UIViewController {
+class ProfileScreenViewController: UIViewController {
 
-    @IBAction func button(_ sender: UIButton) {
-
-        let myViewController = BookDetailViewController(nibName: "BookDetailViewController", bundle: nil)
-        self.present(myViewController, animated: true, completion: nil)
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func viewAllMyOrders(_ sender: UIButton) {
+        let viewAllOrdersController = MyOrdersViewController(nibName: "MyOrdersViewController", bundle: nil)
+//        self.present(viewAllOrdersController, animated: true, completion: nil)
+        viewAllOrdersController.navigationItem.title = "My orders"
+        navigationController?.pushViewController(viewAllOrdersController, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
