3868d9ed035694da109028d373d367ef2d1be08f
huynhthu1704 <huynhthingocthu1704@gmail.com>
2022-05-16 04:35:37 -0700

    edit BookDetail

--- Group1_OnlineBookStore/BookDetailViewController.swift
+++ Group1_OnlineBookStore/BookDetailViewController.swift
@@ -9,36 +9,35 @@
 //      self.present(myViewController, animated: true, completion: nil)
 import UIKit
 
-class BookDetailViewController: UIViewController{
-
+class BookDetailViewController: UIViewController, UITableViewDataSource{
+    
     @IBOutlet weak var reviewTableView: UITableView!
     var reviews = [Review]()
-       override func viewDidLoad() {
-           super.viewDidLoad()
-           if  let review = Review(username: "thuhuynh", content: "It is fantastic", ratingValue: 4) {
-                  reviews.append(review)
-//           }
-//        self.reviewTableView.register(UINib(nibName: "ReviewTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
-//
-//           reviewTableView.dataSource = self
-
-           // Do any additional setup after loading the view.
-       }
-//       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//           reviews.count
-//       }
-//
-//       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//           let reuseIndentifier = "cell"
-//           if let cell = reviewTableView.dequeueReusableCell(withIdentifier: reuseIndentifier, for: indexPath) as? ReviewTableViewCell {
-//               let review = reviews[indexPath.row]
-//                              cell.username.text = review.username
-//                              cell.content.text = review.content
-//                              cell.ratingControl.setRatingValue(ratingValue: review.ratingValue)
-//                              return cell
-//                          } else {
-//                              fatalError("Can not create the cell")
-//                          }
-//
-       }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if  let review = Review(username: "thuhuynh", content: "It is fantastic", ratingValue: 4) {
+            reviews.append(review)          }
+        self.reviewTableView.register(UINib(nibName: "ReviewTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        
+        reviewTableView.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        reviews.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reuseIndentifier = "cell"
+        if let cell = reviewTableView.dequeueReusableCell(withIdentifier: reuseIndentifier, for: indexPath) as? ReviewTableViewCell {
+            let review = reviews[indexPath.row]
+            cell.username.text = review.username
+            cell.content.text = review.content
+            cell.ratingControl.setRatingValue(ratingValue: review.ratingValue)
+            return cell
+        } else {
+            fatalError("Can not create the cell")
+        }
+        
+    }
 }
