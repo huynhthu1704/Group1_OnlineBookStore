ec970d46c622046d4d94381d1a490c582736945a
voyen057 <voyen057@gmail.com>
2022-05-23 03:04:40 -0700

    category admin

--- Group1_OnlineBookStore/View/AdminCategoryViewController.swift
+++ Group1_OnlineBookStore/View/AdminCategoryViewController.swift
@@ -8,15 +8,34 @@
 
 import UIKit
 
-class AdminCategoryViewController: UIViewController {
-
+class AdminCategoryViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = (tableView.dequeueReusableCell(withIdentifier: "CategoryTableViewCell") as? CategoryTableViewCell)!
+        cell.img.image = UIImage(named: "XuXu")
+        cell.lblName.text = "Xu xu dung khoc"
+        return cell
+    
+    }
+    
+    var categories = [Category]()
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        tableView.delegate = self
+        tableView.dataSource = self
         tableView.register(UINib(nibName: "CategoryTableViewCell", bundle: nil), forCellReuseIdentifier: "CategoryTableViewCell")
-
+let category = Category(id: "123", name: "Ke tam xuong", img: UIImage(named: "XuXu"))
         // Do any additional setup after loading the view.
+        for _ in 1...8{
+            categories.append(category)
+        }
     }
 
 
